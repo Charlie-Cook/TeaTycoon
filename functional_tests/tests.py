a3a6cf050083556dcf4b072c914df9c8147960d1
Charlie Cook <charles.cook@digital.hmrc.gov.uk>
2016-01-04 15:56:56 +0000

    Updated functional tests for adding members + supplies.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -17,3 +17,19 @@ class AdminVisitorTest(LiveServerTestCase):
     def test_viewing_the_page(self):
         self.browser.get(self.live_server_url)
         self.assertIn('Tea Tycoon', self.browser.title)
+
+    def test_adding_new_member(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_member')
+        inputbox.send_keys('Charlie Cook')
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_members_table')
+        self.assertIn('Charlie Cook', table.text)
+
+    def test_adding_new_supply(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_supply')
+        inputbox.send_keys('Teabags')
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_supplies_table')
+        self.assertIn('Teabags', table.text)
