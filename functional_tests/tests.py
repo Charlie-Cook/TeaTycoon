466fd18b1bc10aef91c7d10f2e0375ee256b7e60
Charlie Cook <charles.cook@digital.hmrc.gov.uk>
2016-01-04 17:39:01 +0000

    Added functional tests for collection/purchasing and updated homepage.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -33,3 +33,25 @@ class AdminVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         table = self.browser.find_element_by_id('id_supplies_table')
         self.assertIn('Teabags', table.text)
+
+    def test_collecting_from_member(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_member')
+        inputbox.send_keys('Charlie Cook')
+        inputbox.send_keys(Keys.ENTER)
+        # id_collect_1 is equal to the ID of the collect button of user 1
+        collect_button = self.browser.find_element_by_link_text('Collect')
+        collect_button.click()
+        table = self.browser.find_element_by_id('id_members_table')
+        self.assertIn("Paid", table.text)
+
+    def test_purchasing_supplies(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_supply')
+        inputbox.send_keys('Teabags')
+        inputbox.send_keys(Keys.ENTER)
+        # id_collect_1 is equal to the ID of the collect button of user 1
+        purchase_button = self.browser.find_element_by_link_text('Purchase')
+        purchase_button.click()
+        table = self.browser.find_element_by_id('id_supplies_table')
+        self.assertIn("In Stock", table.text)
